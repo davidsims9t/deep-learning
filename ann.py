974f1e61293409224bc998e4baa3158ab3732722
David Sims <davidesims@icloud.com>
2017-08-13 21:41:10 -0700

    finished step 2 and 3

--- ann.py
+++ ann.py
@@ -1,9 +1,11 @@
+# Part 1 - Data Processing
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 
+# Import data set
 dataset = pd.read_csv('Churn_Modelling.csv')
 
 # All indexes from 3 - 12
@@ -31,3 +33,40 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+# Step 2 - Make the ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialial the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+# 11 Independent variables
+# Relu = Rectifier function
+
+# First hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+# Second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compile ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fit the ANN to the training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+# Step 3
+
+# Predict the test results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
