2fd9ecafd2843cafb143da1b87044459a745c2b0
David Sims <davidesims@icloud.com>
2017-12-17 19:49:53 -0800

    convert training and test set into users rows and columns

--- autoencoders/autoencoders.py
+++ autoencoders/autoencoders.py
@@ -8,14 +8,29 @@ import torch.utils.data
 from torch.autograd import Variable
 
 # Importing the dataset
-movies = pd.read_csv('../boltzmann/ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-users = pd.read_csv('../boltzmann/ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-ratings = pd.read_csv('../boltzmann/ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 
 # Preparing the training set
-training_set = pd.read_csv('../boltzmann/ml-100k/u1.base', delimiter = '\t')
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
 
 # Preparing the test set
-test_set = pd.read_csv('../boltzmann/ml-100k/u1.test', delimiter = '\t')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
+
+# Getting the number of users and movies
+nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
+nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
+
+# Convert the data into an array w/ users in rows and movies in columns
+def convert(data):
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
